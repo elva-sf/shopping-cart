992b10e91f89d93a06ba7eb81ed963d834cec06e
elva <e.souto.fernandez@gmail.com>
2020-02-13 18:36:31 +0100

    add docs folder

---
+++ docs/precache-manifest.5f8e930155664b66b359cb4caa1438dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a0a2547af14d6924225705dd0b99222",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ed75b6d3050fd831a43e",
+    "url": "./static/css/main.56758563.chunk.css"
+  },
+  {
+    "revision": "c6d6ca4a056b26b171a4",
+    "url": "./static/js/2.3ad13180.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.3ad13180.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed75b6d3050fd831a43e",
+    "url": "./static/js/main.81b2fdd8.chunk.js"
+  },
+  {
+    "revision": "04888d95dcb71134e21a",
+    "url": "./static/js/runtime-main.7f2ab671.js"
+  },
+  {
+    "revision": "778660f256a9687b256d247b22ba0d2f",
+    "url": "./static/media/background.778660f2.png"
+  }
+]);
--- src/services/products.js
+++ src/services/products.js
@@ -1,5 +1,5 @@
 export default () => {
-  return fetch("http://localhost:3000/api/products.json")
+  return fetch("https://localhost:3000/api/products.json")
     .then(response => response.json())
     .then(responseData => responseData.products);
 };
